1212c713879e474d73ae1f90e68018f06fa67270
Hlayi2 <katlegokatenguyuza01@gmail.com>
2023-07-28 13:22:04 -0700

    Strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter
- * and updates the value it points to to 98
- * @n: input
- * Return: n
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * Return: void
  */
-void reset_to_98(int *n)
+
+char *_strcat(char *dest, char *src)
 {
-	*n = 98;
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
+
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
+
+return (dest);
 }
